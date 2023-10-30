c7fec64dfcfa582957203ba46c2c526b9c030de3
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-10-30 17:17:29 +0900

    feat: チャットボタンを実装

--- anitection/lib/screens/institution_detail/institution_screen.dart
+++ anitection/lib/screens/institution_detail/institution_screen.dart
@@ -2,6 +2,7 @@ import 'package:anitection/components/nav_up_button.dart';
 import 'package:anitection/components/stroke_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../components/animal_pad_background.dart';
 
@@ -57,67 +58,70 @@ class InstitutionScreenState extends ConsumerState<InstitutionScreen> {
           const AnimalPadBackground(),
           // ここに他のウィジェットを追加
           SingleChildScrollView(
-              child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const InstitutionImage(
-                  url:
-                      "https://img.furusato-tax.jp/img/x/original/feature/form/details/20211129/gpfd_107bfa9e0ad3c76c4afea5461d79acf480140bda.jpg",
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                const InstitutionLabel(label: "所在地"),
-                const SizedBox(
-                  height: 8,
-                ),
-                _Address(
-                  address: "大阪府大阪市旭区高殿2-21-11-5",
-                  onCopyToClipboard: () {},
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                const InstitutionLabel(label: "電話番号"),
-                const SizedBox(
-                  height: 8,
-                ),
-                _Tel(
-                  tel: "222-2222-2222",
-                  onCopyToClipboard: () {},
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                const InstitutionLabel(label: "Webサイト"),
-                const SizedBox(
-                  height: 8,
-                ),
-                _WebSite(url: "http://www/nyannyan.com", onOpenWebSite: () {}),
-                const SizedBox(
-                  height: 16,
-                ),
-                const InstitutionLabel(label: "取扱動物"),
-                const SizedBox(
-                  height: 8,
-                ),
-                const _Text(text: "犬 23匹 / 猫 12匹 / 鳥 4匹 / "),
-                const SizedBox(
-                  height: 16,
-                ),
-                const InstitutionLabel(label: "ほしい物リスト"),
-                const SizedBox(
-                  height: 8,
-                ),
-                const _Text(text: "Amazon"),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const InstitutionImage(
+                    url:
+                        "https://img.furusato-tax.jp/img/x/original/feature/form/details/20211129/gpfd_107bfa9e0ad3c76c4afea5461d79acf480140bda.jpg",
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  const InstitutionLabel(label: "所在地"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  _Address(
+                    address: "大阪府大阪市旭区高殿2-21-11-5",
+                    onCopyToClipboard: () {},
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const InstitutionLabel(label: "電話番号"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  _Tel(
+                    tel: "222-2222-2222",
+                    onCopyToClipboard: () {},
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const InstitutionLabel(label: "Webサイト"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  _WebSite(
+                      url: "http://www/nyannyan.com", onOpenWebSite: () {}),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const InstitutionLabel(label: "取扱動物"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Text(text: "犬 23匹 / 猫 12匹 / 鳥 4匹 / "),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const InstitutionLabel(label: "ほしい物リスト"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const _Text(text: "Amazon"),
+                ],
+              ),
             ),
-          ))
+          )
         ],
       ),
+      floatingActionButton: _ChatButton(),
     );
   }
 }
@@ -328,3 +332,65 @@ class _Text extends StatelessWidget {
   }
 }
 
+class _ChatButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+            bottom: -8,
+            child: Container(
+              width: 90,
+              height: 90,
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color(0xFFD9D9D9),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2,
+                ),
+              ),
+            )),
+        Container(
+          width: 90,
+          height: 90,
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: const Color(0xFFFFDF90),
+            border: Border.all(
+              color: Colors.white,
+              width: 4,
+            ),
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: 10,
+                child: SvgPicture.asset(
+                  "assets/svg/ic_chat_and_shadow.svg",
+                  width: 40,
+                  height: 32,
+                ),
+              ),
+              const Positioned(
+                  top: 32,
+                  child: StrokeText(
+                    text: "チャット",
+                    textColor: Color(0xFFC39248),
+                    strokeColor: Colors.white,
+                    strokeWidth: 2,
+                    textStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
