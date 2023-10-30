390a2477c22fec90bcd60fc04c195d2769eb5ed7
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-10-30 10:36:56 +0900

    feat: 画面を作成

---
+++ anitection/lib/screens/institution_detail/institution_screen.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class InstitutionScreen extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
