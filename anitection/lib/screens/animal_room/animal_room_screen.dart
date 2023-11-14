f66e2c58de85bac2322513561979eab487d7dc85
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-11-14 10:50:41 +0900

    feat: format

--- anitection/lib/screens/animal_room/animal_room_screen.dart
+++ anitection/lib/screens/animal_room/animal_room_screen.dart
@@ -21,74 +21,75 @@ class AnimalRoomScreenState extends ConsumerState<AnimalRoomScreen> {
         title: const Text("Animal Room"),
       ),
       body: Center(
-          child: ElevatedButton(
-              onPressed: () {
-                showModalBottomSheet(
-                  isScrollControlled: true,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
+        child: ElevatedButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              context: context,
+              builder: (context) {
+                return FractionallySizedBox(
+                  heightFactor: 0.75,
+                  child: Container(
+                    height: size.height * 0.9,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                        border: Border(
+                          top: BorderSide(
+                            width: 5,
+                            color: Color(0xFFC3EB89),
+                          ),
+                          left: BorderSide(
+                            width: 5,
+                            color: Color(0xFFC3EB89),
+                          ),
+                          right: BorderSide(
+                            width: 5,
+                            color: Color(0xFFC3EB89),
+                          ),
+                          bottom: BorderSide(
+                            width: 0,
+                            color: Color(0xFFC3EB89),
+                          ),
+                        )),
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      alignment: Alignment.topCenter,
+                      children: [
+                        Positioned(
+                          top: -60,
+                          child: Container(
+                            height: 120,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(60),
+                              border: Border.all(
+                                color: const Color(0xFFC3EB89),
+                                width: 5,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  context: context,
-                  builder: (context) {
-                    return FractionallySizedBox(
-                      heightFactor: 0.75,
-                      child: Container(
-                        height: size.height * 0.9,
-                        decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(20),
-                              topRight: Radius.circular(20),
-                            ),
-                            border: Border(
-                              top: BorderSide(
-                                width: 5,
-                                color: Color(0xFFC3EB89),
-                              ),
-                              left: BorderSide(
-                                width: 5,
-                                color: Color(0xFFC3EB89),
-                              ),
-                              right: BorderSide(
-                                width: 5,
-                                color: Color(0xFFC3EB89),
-                              ),
-                              bottom: BorderSide(
-                                width: 0,
-                                color: Color(0xFFC3EB89),
-                              ),
-                            )
-                        ),
-                        child: Stack(
-                          clipBehavior: Clip.none,
-                          alignment: Alignment.topCenter,
-                          children: [
-                            Positioned(
-                              top: -60,
-                              child: Container(
-                                height: 120,
-                                width: 120,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(60),
-                                  border: Border.all(
-                                    color: const Color(0xFFC3EB89),
-                                    width: 5,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
                 );
               },
-              child: const Text("Button"))),
+            );
+          },
+          child: const Text("Button"),
+        ),
+      ),
     );
   }
 }
