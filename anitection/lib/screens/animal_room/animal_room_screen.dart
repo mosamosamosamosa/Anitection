4b4c07997c24e65f3d03d0750cdbb500e7685bbb
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-11-14 10:47:35 +0900

    feat: 画面のベースを作成

---
+++ anitection/lib/screens/animal_room/animal_room_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AnimalRoomScreen extends ConsumerStatefulWidget {
+  const AnimalRoomScreen({super.key, required this.animalId});
+
+  final int animalId;
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return AnimalRoomScreenState();
+  }
+}
+
+class AnimalRoomScreenState extends ConsumerState<AnimalRoomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Animal Room"),
+      ),
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      height: size.height * 0.9,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                        border: Border(
+                          top: BorderSide(
+                            width: 5,
+                            color: Color(0xFFC3EB89),
+                          ),
+                          left: BorderSide(
+                            width: 5,
+                            color: Color(0xFFC3EB89),
+                          ),
+                          right: BorderSide(
+                            width: 5,
+                            color: Color(0xFFC3EB89),
+                          ),
+                          bottom: BorderSide(
+                            width: 0,
+                            color: Color(0xFFC3EB89),
+                          ),
+                        )
+                      ),
+                      child: Stack(
+                        clipBehavior: Clip.none,
+                        alignment: Alignment.topCenter,
+                        children: [
+                          Positioned(
+                            top: -60,
+                            child: Container(
+                              height: 120,
+                              width: 120,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(60),
+                                border: Border.all(
+                                  color: const Color(0xFFC3EB89),
+                                  width: 5,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+              child: const Text("Button"))),
+    );
+  }
+}
