e869cc0addeac5331f742475a0077514742f198c
yumekiti <yumekiti120440@gmail.com>
2023-11-16 13:38:15 +0900

    feat(web): format

--- web/src/components/templates/Layout.tsx
+++ web/src/components/templates/Layout.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Component: React.FC<Props> = ({ children }) => {
   return (
     <div className="App h-screen flex flex-col">
-      <header className='bg-stroke p-2 flex justify-between items-center'>
+      <header className="bg-stroke p-2 flex justify-between items-center">
         <h1 className="py-4 text-2xl font-bold">Anitection</h1>
         <div className="flex gap-4">
           <a href="/login">ログイン</a>
