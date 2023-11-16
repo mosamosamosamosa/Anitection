2f64c9d200958a0b4f26eddef097f9b8a81af432
yumekiti <yumekiti120440@gmail.com>
2023-11-16 13:38:02 +0900

    feat(web): update tailiwind colors

--- web/src/components/templates/Layout.tsx
+++ web/src/components/templates/Layout.tsx
@@ -9,7 +9,13 @@ type Props = {
 const Component: React.FC<Props> = ({ children }) => {
   return (
     <div className="App h-screen flex flex-col">
-      <header>sdsadf</header>
+      <header className='bg-stroke p-2 flex justify-between items-center'>
+        <h1 className="py-4 text-2xl font-bold">Anitection</h1>
+        <div className="flex gap-4">
+          <a href="/login">ログイン</a>
+          <a href="/signup">新規登録</a>
+        </div>
+      </header>
       <main className="flex-grow">{children}</main>
     </div>
   );
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#fffffe',
+        headline: '#272343',
+        paragraph: '#2d334a',
+        button: '#ffd803',
+        'button-text': '#272343',
+        stroke: '#C3EB89',
+        main: '#fffffe',
+        highlight: '#ffd803',
+        secondary: '#e3f6f5',
+        tertiary: '#bae8e8',
+      },
+    },
   },
   plugins: [],
 };
