d75b8c66308a7776cfbc3e8011577ae776555eee
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-11-24 00:33:03 +0900

    feat: 雑に実装

--- anitection/lib/screens/initial_profile/initial_profile_editor_screen.dart
+++ anitection/lib/screens/initial_profile/initial_profile_editor_screen.dart
@@ -1,4 +1,5 @@
 import 'package:anitection/components/animal_pad_background.dart';
+import 'package:anitection/components/normal_button.dart';
 import 'package:anitection/components/stroke_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -14,6 +15,9 @@ class InitialProfileEditorScreen extends ConsumerStatefulWidget {
 
 class InitialProfileEditorScreenState
     extends ConsumerState<InitialProfileEditorScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _nicknameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +29,7 @@ class InitialProfileEditorScreenState
           strokeWidth: 4,
           strokeColor: Colors.white,
           textStyle: TextStyle(
-              color: Color(0xFF573F1B),
+            color: Color(0xFF573F1B),
             fontSize: 24,
             fontWeight: FontWeight.bold,
           ),
@@ -40,20 +44,298 @@ class InitialProfileEditorScreenState
       ),
       body: Stack(
         children: [
-          AnimalPadBackground(),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          const AnimalPadBackground(),
+          Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: InitialProfileEditorScreenAvatarPreview(),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  child: Column(
+                    children: [
+                      const NicknameHeader(),
+                      TextFormField(
+                        controller: _nicknameController,
+                        decoration: const InputDecoration(
+                          hintText: "ニックネームを入力してください",
+                          hintStyle: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF573F1B),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF573F1B),
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF573F1B),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      const LiveHeader(),
+                      TextFormField(
+                        controller: _nicknameController,
+                        decoration: const InputDecoration(
+                          hintText: "お住まいの地域を入力してください",
+                          hintStyle: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF573F1B),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF573F1B),
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF573F1B),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      const InstallReasonHeader(),
 
-            children: [
-              Stack(
-                children: [
-
-                ],
-              )
-            ],
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CancelButton(onPressed: () {}),
+                      const SizedBox(width: 16),
+                      SubmitButton(onPressed: () {})
+                    ],
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
+    );
+  }
+}
+
+class CancelButton extends StatelessWidget {
+  const CancelButton({super.key, required this.onPressed});
+
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return NormalButton(
+      width: 160,
+      height: 56,
+      surfaceColor: const Color(0xFFC7C7C7),
+      sideColor: const Color(0xFF888888),
+      maxElevation: 6,
+      onPressed: onPressed,
+      child: const StrokeText(
+        text: "キャンセル",
+        strokeWidth: 4,
+        strokeColor: Colors.white,
+        textStyle: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF573F1B),
+        ),
+      ),
+    );
+  }
+}
+
+class SubmitButton extends StatelessWidget {
+  const SubmitButton({super.key, required this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return NormalButton(
+      width: 160,
+      height: 56,
+      maxElevation: 6,
+      onPressed: onPressed,
+      child: const StrokeText(
+        text: "登録",
+        strokeWidth: 4,
+        strokeColor: Colors.white,
+        textStyle: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF573F1B),
+        ),
+      ),
+    );
+  }
+}
+class InitialProfileEditorScreenAvatarPreview extends StatelessWidget {
+  const InitialProfileEditorScreenAvatarPreview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: const Color(0xFFFFB001),
+              width: 3,
+            ),
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage("assets/images/img_empty_avatar.png"),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: Container(
+            width: 30,
+            height: 30,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0xFFFFB001),
+            ),
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+              size: 20,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class RequireLabel extends StatelessWidget {
+  const RequireLabel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        color: const Color(0xFFFF635A),
+      ),
+      child: const Text(
+        "必須",
+        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+    );
+  }
+}
+
+class InstallReasonHeader extends StatelessWidget {
+  const InstallReasonHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        StrokeText(
+          text: "アプリを入れた理由",
+          strokeWidth: 4,
+          strokeColor: Colors.white,
+          textStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFF573F1B),
+          ),
+          shadows: [
+            Shadow(
+              color: Colors.black,
+              offset: Offset(2, 2),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        SizedBox(width: 8),
+        RequireLabel(),
+      ],
+    );
+  }
+}
+
+class LiveHeader extends StatelessWidget {
+  const LiveHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        StrokeText(
+          text: "お住まいの地域",
+          strokeWidth: 4,
+          strokeColor: Colors.white,
+          textStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFF573F1B),
+          ),
+          shadows: [
+            Shadow(
+              color: Colors.black,
+              offset: Offset(2, 2),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        SizedBox(width: 8),
+        RequireLabel(),
+      ],
+    );
+  }
+}
+
+class NicknameHeader extends StatelessWidget {
+  const NicknameHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        StrokeText(
+          text: "ニックネーム",
+          strokeWidth: 4,
+          strokeColor: Colors.white,
+          textStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFF573F1B),
+          ),
+          shadows: [
+            Shadow(
+              color: Colors.black,
+              offset: Offset(2, 2),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        SizedBox(width: 8),
+        RequireLabel(),
+      ],
     );
   }
 }
