cc8e7ffdc9d071b1bfd68179ee7cb0ed6dd3462a
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-11-27 11:34:33 +0900

    feat: appBarのひらひら部分を描画できるようにした

--- anitection/lib/screens/animal_room/animal_room_screen.dart
+++ anitection/lib/screens/animal_room/animal_room_screen.dart
@@ -6,6 +6,7 @@ import 'package:anitection/providers/animal.dart';
 import 'package:anitection/screens/animal_room/animal_room_profile_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AnimalRoomScreen extends ConsumerStatefulWidget {
   const AnimalRoomScreen({super.key, required this.animalId});
@@ -24,24 +25,17 @@ class AnimalRoomScreenState extends ConsumerState<AnimalRoomScreen> {
     final animalAsyncState = ref.watch(animalFutureProvider(widget.animalId));
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Animal Room"),
+      appBar: PreferredSize(
+        child: Container(
+          color: Color(0xFFC3EB89),
+          child: Column(children: []),
+        ),
+        preferredSize: Size.fromHeight(size.height * 0.1),
       ),
-      body: Center(
-        child: animalAsyncState.when(data: (data) {
-          return ElevatedButton(
-            onPressed: () {
-              showAnimalRoomProfileDialog(context, size, data.data);
-            },
-            child: const Text("Button"),
-          );
-        }, error: (e, st) {
-          log("error", error: e, stackTrace: st);
-          return const Text("error");
-        }, loading: () {
-          return const CircularProgressIndicator();
-        }),
-      ),
+      body: Stack(children: [
+        const AnimalRoomBackground(),
+        AppBarCurtain(),
+      ]),
     );
   }
 }
@@ -70,4 +64,75 @@ Future<void> showAnimalRoomProfileDialog(
       );
     },
   );
+}
+
+class AnimalRoomBackground extends StatelessWidget {
+  const AnimalRoomBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset("assets/svg/img_animal_room_background.svg",
+        fit: BoxFit.cover);
+  }
+}
+
+class AppBarCurtainItem extends StatelessWidget {
+  const AppBarCurtainItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 39,
+      width: 50,
+      decoration: BoxDecoration(
+          color: Color(0xFFC3EB89),
+          borderRadius: BorderRadius.all(
+            Radius.elliptical(50, 39),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              offset: Offset(0, 4),
+              blurRadius: 2,
+            )
+          ]),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 19,
+            left: 20,
+            child: Container(
+              width: 10,
+              height: 10,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AppBarCurtain extends StatelessWidget {
+  const AppBarCurtain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    // ignore: unnecessary_null_comparison
+    final count = size == null ? 0 : size.width ~/ 50;
+
+    return Container(
+      clipBehavior: Clip.none,
+      child: Stack(
+        children: List.generate(count + 2, (index) {
+          return Positioned(
+              top: -19, left: index * 50 - 25, child: const AppBarCurtainItem());
+        }),
+      ),
+    );
+  }
 }
