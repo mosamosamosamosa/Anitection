68632424f66e82432bb6f3907cd88f82e0005d4a
yumekiti <yumekiti120440@gmail.com>
2023-11-28 12:15:57 +0900

    refactor: code formatting

--- web/src/components/organisms/Login.tsx
+++ web/src/components/organisms/Login.tsx
@@ -22,27 +22,35 @@ const Component: FC = () => {
   };
 
   return (
-    <Modal text='ログイン' handleClick={handleCloseClick}>
-      <div className='flex flex-col justify-center items-center gap-4 mb-12'>
-        <div className='flex flex-col justify-center items-start'>
-          <p className='font-bold'>メールアドレス</p>
-          <input type='text' className='w-64 h-8 rounded-md border border-gray-300 px-1 outline-none focus:border-highlight' />
+    <Modal text="ログイン" handleClick={handleCloseClick}>
+      <div className="flex flex-col justify-center items-center gap-4 mb-12">
+        <div className="flex flex-col justify-center items-start">
+          <p className="font-bold">メールアドレス</p>
+          <input
+            type="text"
+            className="w-64 h-8 rounded-md border border-gray-300 px-1 outline-none focus:border-highlight"
+          />
         </div>
-        <div className='flex flex-col justify-center items-start'>
-          <p className='font-bold'>パスワード</p>
-          <input type='password' className='w-64 h-8 rounded-md border border-gray-300 px-1 outline-none focus:border-highlight' />
+        <div className="flex flex-col justify-center items-start">
+          <p className="font-bold">パスワード</p>
+          <input
+            type="password"
+            className="w-64 h-8 rounded-md border border-gray-300 px-1 outline-none focus:border-highlight"
+          />
         </div>
       </div>
-      <div className='flex flex-col justify-center items-center'>
-        <button className='w-64 h-8 rounded-md bg-highlight text-white font-bold'>ログイン</button>
-        <div className='flex flex-col justify-center items-center'>
-          <p className='text-sm mt-2' onClick={handleRegisterClick}>
+      <div className="flex flex-col justify-center items-center">
+        <button className="w-64 h-8 rounded-md bg-highlight text-white font-bold">
+          ログイン
+        </button>
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-sm mt-2" onClick={handleRegisterClick}>
             新規作成はこちら
           </p>
-          <p className='text-sm mt-4 ml-4' onClick={handleResetClick}>
+          <p className="text-sm mt-4 ml-4" onClick={handleResetClick}>
             パスワードを忘れた方はこちら
           </p>
-          <p className='text-red-500 text-sm mt-10 ml-4'>&nbsp;{message}</p>
+          <p className="text-red-500 text-sm mt-10 ml-4">&nbsp;{message}</p>
         </div>
       </div>
     </Modal>
--- web/src/components/organisms/Register.tsx
+++ web/src/components/organisms/Register.tsx
@@ -17,26 +17,40 @@ const Component: FC = () => {
   };
 
   return (
-    <Modal text='登録' handleClick={handleCloseClick}>
-      <div className='flex flex-col justify-center items-center gap-4 mb-12'>
-        <div className='flex flex-col justify-center items-start'>
-          <p className='text-headline font-bold'>メールアドレス</p>
-          <input type='text' className='w-64 h-8 rounded-md border border-gray-300 px-1' />
+    <Modal text="登録" handleClick={handleCloseClick}>
+      <div className="flex flex-col justify-center items-center gap-4 mb-12">
+        <div className="flex flex-col justify-center items-start">
+          <p className="text-headline font-bold">メールアドレス</p>
+          <input
+            type="text"
+            className="w-64 h-8 rounded-md border border-gray-300 px-1"
+          />
         </div>
-        <div className='flex flex-col justify-center items-start'>
-          <p className='text-headline font-bold'>パスワード</p>
-          <input type='password' className='w-64 h-8 rounded-md border border-gray-300 px-1' />
+        <div className="flex flex-col justify-center items-start">
+          <p className="text-headline font-bold">パスワード</p>
+          <input
+            type="password"
+            className="w-64 h-8 rounded-md border border-gray-300 px-1"
+          />
         </div>
-        <div className='flex flex-col justify-center items-start'>
-          <p className='text-headline font-bold'>パスワード確認</p>
-          <input type='password' className='w-64 h-8 rounded-md border border-gray-300 px-1' />
+        <div className="flex flex-col justify-center items-start">
+          <p className="text-headline font-bold">パスワード確認</p>
+          <input
+            type="password"
+            className="w-64 h-8 rounded-md border border-gray-300 px-1"
+          />
         </div>
       </div>
-      <div className='flex flex-col justify-center items-center'>
-        <button className='w-64 h-8 rounded-md bg-icons-tertiary text-white font-bold' onClick={handleLoginClick}>
+      <div className="flex flex-col justify-center items-center">
+        <button
+          className="w-64 h-8 rounded-md bg-icons-tertiary text-white font-bold"
+          onClick={handleLoginClick}
+        >
           登録
         </button>
-        <p className='text-icons-tertiary text-sm mt-10 ml-4'>&nbsp;{message}</p>
+        <p className="text-icons-tertiary text-sm mt-10 ml-4">
+          &nbsp;{message}
+        </p>
       </div>
     </Modal>
   );
