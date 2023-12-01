2f0ae837a3aeee6a08ea852732ba18af2be53046
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-12-01 16:46:05 +0900

    feat: 点線の描画をできるようにした

--- anitection/lib/screens/home/home_screen.dart
+++ anitection/lib/screens/home/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:anitection/components/animal_pad_background.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+// import math
+import 'dart:math' as math;
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -115,8 +117,58 @@ class TrianglePainter extends CustomPainter {
     canvas.drawShadow(path, Colors.black.withOpacity(0.5), 4.0, false);
 
     canvas.drawPath(path, paint);
+
+    // 点線を描画
+    // var dotPathPaint = Paint()
+    //   ..color = Colors.white
+    //   ..strokeWidth = 4;
+    //
+    // double dashWidth = 5.0, dashSpace = 6.0, startX = 0.0;
+    // while (startX < size.width) {
+    //   canvas.drawLine(Offset(startX, 0), Offset(startX + dashWidth, 0), dotPathPaint);
+    //   startX += dashWidth + dashSpace;
+    // }
+    //
+    // final paint = Paint()
+    //   ..color = Colors.black
+    //   ..strokeWidth = 1;
+
+    final dotLinePaint = Paint()
+      ..color = Colors.white
+      ..strokeWidth = 2;
+
+    const dashWidth = 5.0;
+    const dashSpace = 3.0;
+    void drawDashedLine(Offset start, Offset end) {
+      final dx = end.dx - start.dx;
+      final dy = end.dy - start.dy;
+      final distance = math.sqrt(dx * dx + dy * dy);
+      final dashCount = (distance / (dashWidth + dashSpace)).floor();
+
+      for (var i = 0; i < dashCount; ++i) {
+        if (i % 2 == 0) {
+          final x1 = start.dx + dx / dashCount * (i + dashWidth / distance);
+          final y1 = start.dy + dy / dashCount * (i + dashWidth / distance);
+          final x2 = start.dx + dx / dashCount * ((i + 1) + dashWidth / distance);
+          final y2 = start.dy + dy / dashCount * ((i + 1) + dashWidth / distance);
+          canvas.drawLine(Offset(x1, y1), Offset(x2, y2), dotLinePaint);
+        }
+      }
+    }
+
+    // 三角形の頂点を計算
+    final point1 = Offset(size.width / 2, size.height);
+    final point2 = Offset(0, 0);
+    final point3 = Offset(size.width, 0);
+
+    // 各辺に点線を描画
+    drawDashedLine(point1, point2);
+    drawDashedLine(point3, point1);
+
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
+
+
