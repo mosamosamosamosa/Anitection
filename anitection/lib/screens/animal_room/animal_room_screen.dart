7eb43dfb7309fb373605a8000ac6fe8c8478a4ac
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-12-01 21:37:50 +0900

    feat: 前の画面に戻れるようにした

--- anitection/lib/screens/animal_room/animal_room_screen.dart
+++ anitection/lib/screens/animal_room/animal_room_screen.dart
@@ -67,10 +67,14 @@ class AnimalRoomScreenState extends ConsumerState<AnimalRoomScreen> {
               ),
             ),
           ),
-          const Positioned(
+          Positioned(
             bottom: -83,
             left: -116,
-            child: DoorIcon(),
+            child: DoorIcon(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
           ),
           const Positioned(
             bottom: 0,
@@ -224,39 +228,43 @@ class AnimalRoomBottomNavMenu extends StatelessWidget {
 }
 
 class DoorIcon extends StatelessWidget {
-  const DoorIcon({super.key});
+  const DoorIcon({super.key, required this.onPressed});
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // 楕円
-      width: 209,
-      height: 177,
-      decoration: BoxDecoration(
-        color: const Color(0xFFE4E4E4),
-        borderRadius: const BorderRadius.all(
-          Radius.elliptical(209, 177),
+    return GestureDetector(
+      onTap:onPressed,
+      child: Container(
+        // 楕円
+        width: 209,
+        height: 177,
+        decoration: BoxDecoration(
+          color: const Color(0xFFE4E4E4),
+          borderRadius: const BorderRadius.all(
+            Radius.elliptical(209, 177),
+          ),
+          border: Border.all(
+            color: const Color(0xFFFFFFFF),
+            width: 3,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              offset: const Offset(4, -4),
+              blurRadius: 4,
+            )
+          ],
         ),
-        border: Border.all(
-          color: const Color(0xFFFFFFFF),
-          width: 3,
+        child: Stack(
+          children: [
+            Positioned(
+              top: 27,
+              right: 34,
+              child: SvgPicture.asset("assets/svg/ic_door.svg"),
+            )
+          ],
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            offset: const Offset(4, -4),
-            blurRadius: 4,
-          )
-        ],
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            top: 27,
-            right: 34,
-            child: SvgPicture.asset("assets/svg/ic_door.svg"),
-          )
-        ],
       ),
     );
   }
