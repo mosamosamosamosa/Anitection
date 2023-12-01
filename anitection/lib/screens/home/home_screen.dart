5b9e1c14ab98c42c10f1366637228a56fc0a99a9
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-12-01 17:57:52 +0900

    feat: アバターのガワを描画できるようにした

--- anitection/lib/screens/home/home_screen.dart
+++ anitection/lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:anitection/components/animal_pad_background.dart';
+import 'package:anitection/components/stroke_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -126,43 +127,65 @@ class HomeScreenState extends ConsumerState<HomeScreen> {
                   ),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 24, right: 24, top: 20),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        SvgPicture.asset("assets/svg/ic_point.svg",
-                            width: 30, height: 30),
-                        const SizedBox(
-                          width: 4,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: const Color(0x40077205),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 6, vertical: 2),
-                          child: const Row(
+              const SizedBox(
+                height: 20,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: PointCounter(),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Flexible(
+                child: ListView.separated(
+                  itemCount: 10,
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+                      child: Row(
+                        children: [
+                          Image.asset("assets/images/img_example_cat_avatar.png", width: 60, height: 60,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
-                                  "10,000",
-                                style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w900)
+                              StrokeText(
+                                  text: "あるるくん",
+                                textStyle: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF444444),
+                                ),
+                                strokeWidth: 2,
+                                strokeColor: Colors.white,
+                                shadows: [
+                                  Shadow(
+                                    blurRadius: 2,
+                                    color: Colors.black.withOpacity(0.5),
+                                    offset: const Offset(1, 1),
+                                  )
+                                ],
                               ),
-                              SizedBox(width: 4,),
-                              Text(
-                                  "p",
-                                  style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w900)
-                              )
+                              const Text("あにまる保護施設", textAlign: TextAlign.end,)
                             ],
                           ),
-                        )
-                      ],
-                    )
-                  ],
+
+                        ],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const SizedBox(
+                      height: 18,
+                    );
+                  }
                 ),
-              )
+              ),
             ],
           )
         ],
@@ -254,4 +277,44 @@ class TriangleLinePainter extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
+
+class PointCounter extends StatelessWidget {
+  const PointCounter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SvgPicture.asset("assets/svg/ic_point.svg", width: 30, height: 30),
+        const SizedBox(
+          width: 4,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0x40077205),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+          child: const Row(
+            children: [
+              Text("10,000",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w900)),
+              SizedBox(
+                width: 4,
+              ),
+              Text("p",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w900))
+            ],
+          ),
+        )
+      ],
+    );
+  }
 }
