502400fdc595eaa7a2d6ec8f2a46474d35bc1c1b
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-12-08 15:22:40 +0900

    feat: apiから取得して表示できるようにした

--- anitection/lib/screens/initial_animal_filter/initial_cat_pattern_selection_screen.dart
+++ anitection/lib/screens/initial_animal_filter/initial_cat_pattern_selection_screen.dart
@@ -1,6 +1,9 @@
 import 'package:anitection/components/animal_pad_background.dart';
 import 'package:anitection/components/number_stepper.dart';
 import 'package:anitection/components/stroke_text.dart';
+import 'package:anitection/models/base.dart';
+import 'package:anitection/models/pattern/pattern.dart';
+import 'package:anitection/providers/pattern.dart';
 import 'package:anitection/screens/initial_animal_filter/initial_cat_preference_selection_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -23,8 +26,12 @@ class InitialCatPatternScreen extends ConsumerStatefulWidget {
 }
 
 class InitialCatPatternState extends ConsumerState<InitialCatPatternScreen> {
+  Set<int> selectedPatterns = {};
+
   @override
   Widget build(BuildContext context) {
+    final asyncPatterns = ref.watch(catPatternsFutureProvider);
+    final patterns = asyncPatterns.valueOrNull?.data ?? [];
     final width = MediaQuery.of(context).size.width * 0.7;
     return Scaffold(
       backgroundColor: const Color(0xFFFFF8E0),
@@ -84,12 +91,29 @@ class InitialCatPatternState extends ConsumerState<InitialCatPatternScreen> {
                         ],
                       ),
                       child: GridView.builder(
-                        itemCount: 100,
+                        itemCount: patterns.length,
                         padding: const EdgeInsets.symmetric(
                             horizontal: 16, vertical: 32),
-                        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 110, crossAxisSpacing: 4, mainAxisSpacing: 4),
+                        gridDelegate:
+                            const SliverGridDelegateWithMaxCrossAxisExtent(
+                                maxCrossAxisExtent: 110,
+                                crossAxisSpacing: 4,
+                                mainAxisSpacing: 4),
                         itemBuilder: (BuildContext context, int index) {
-                          return PatternSelectionItem(selected: index % 5 == 0);
+                          final pattern = patterns[index];
+                          return PatternSelectionItem(
+                            selected: selectedPatterns.contains(pattern.id),
+                            model: pattern,
+                            onSelect: () {
+                              setState(() {
+                                if (selectedPatterns.contains(pattern.id)) {
+                                  selectedPatterns.remove(pattern.id);
+                                } else {
+                                  selectedPatterns.add(pattern.id);
+                                }
+                              });
+                            },
+                          );
                         },
                       ),
                     ),
@@ -120,50 +144,62 @@ class InitialCatPatternState extends ConsumerState<InitialCatPatternScreen> {
 }
 
 class PatternSelectionItem extends StatelessWidget {
-  const PatternSelectionItem({super.key, required this.selected});
+  const PatternSelectionItem(
+      {super.key,
+      required this.selected,
+      required this.model,
+      required this.onSelect});
+
   final bool selected;
+  final Model<PatternAttributes> model;
+  final VoidCallback onSelect;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: selected ? 85 : 80,
-          height:  selected ? 85 : 80,
-          decoration: BoxDecoration(
-            color: const Color(0xFFD9D9D9),
-            borderRadius: BorderRadius.circular(10),
-            border: selected ? Border.all(color: const Color(0xFFFFB001), width: 5) : null,
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 4,
-                color: Colors.black.withOpacity(0.25),
-                offset: const Offset(2, 2),
+    return GestureDetector(
+      onTap: onSelect,
+      child: Column(
+        children: [
+          Container(
+            width: selected ? 85 : 80,
+            height: selected ? 85 : 80,
+            decoration: BoxDecoration(
+              color: const Color(0xFFD9D9D9),
+              borderRadius: BorderRadius.circular(10),
+              border: selected
+                  ? Border.all(color: const Color(0xFFFFB001), width: 5)
+                  : null,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 4,
+                  color: Colors.black.withOpacity(0.25),
+                  offset: const Offset(2, 2),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 2,
+          ),
+          StrokeText(
+            text: model.attributes.name,
+            strokeWidth: 3,
+            strokeColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFF573F1B),
+            ),
+            shadows: const [
+              Shadow(
+                blurRadius: 2,
+                color: Colors.black,
+                offset: Offset(1, 1),
               )
             ],
-          ),
-        ),
-        const SizedBox(
-          height: 2,
-        ),
-        const StrokeText(
-          text: "グレー×全身",
-          strokeWidth: 3,
-          strokeColor: Colors.white,
-          textStyle: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w700,
-            color: Color(0xFF573F1B),
-          ),
-          shadows: [
-            Shadow(
-              blurRadius: 2,
-              color: Colors.black,
-              offset: Offset(1, 1),
-            )
-          ],
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
