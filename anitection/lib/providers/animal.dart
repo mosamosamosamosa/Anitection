556e89293412cce0ec611b1e02150c1989b85675
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-12-12 10:14:00 +0900

    feat: リクエストがループしないようにした

--- anitection/lib/providers/animal.dart
+++ anitection/lib/providers/animal.dart
@@ -2,6 +2,8 @@
 import 'package:anitection/models/animal/animal.dart';
 import 'package:anitection/models/base.dart';
 import 'package:anitection/providers/client.dart';
+import 'package:anitection/screens/initial_animal_filter/initial_cat_preference_selection_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final animalsFutureProvider = FutureProvider.autoDispose((ref) {
@@ -12,6 +14,91 @@ final animalFutureProvider = FutureProvider.autoDispose.family<SingleData<Model<
   return ref.read(anitectionClientProvider).getAnimal(id);
 });
 
-final searchAnimalFutureProvider = FutureProvider.autoDispose.family<PagingData<Model<AnimalAttributes>>, Map<String, dynamic>>((ref, query) {
-  return ref.read(anitectionClientProvider).searchAnimals(query);
+final searchAnimalFutureProvider = FutureProvider.autoDispose.family<PagingData<Model<AnimalAttributes>>, AnimalSearchQueries>((ref, query) {
+  return ref.read(anitectionClientProvider).searchAnimals(query.toQueries());
 });
+
+Map<String, dynamic> buildQueryParameter(
+    String animalKind, {
+      List<String>? size,
+      List<String>? hairLength,
+      Set<Age>? age,
+    }) {
+
+  final queryParams = {
+    "filters[animal_kind][name][\$eq]": animalKind,
+  };
+
+  if (size != null) {
+    for (int i = 0; i < size.length; i ++) {
+      final s = size[i];
+      queryParams["filters[\$or][$i][size][\$eq]"] = s;
+    }
+  }
+
+  if (hairLength != null) {
+    for (int i = 0; i < hairLength.length; i ++) {
+      final l = hairLength[i];
+      queryParams["filters[\$or][$i][hair_length][\$eq]"] = l;
+    }
+  }
+
+  if (age != null) {
+    int min;
+    int max;
+    if (age.contains(Age.kitten)) {
+      min = 0;
+    } else if (age.contains(Age.adult)) {
+      min = 1;
+    } else {
+      min = 6;
+    }
+
+    if (age.contains(Age.senior)) {
+      max = 2147483647;
+    } else if (age.contains(Age.adult)) {
+      max = 11;
+    } else {
+      max = 1;
+    }
+
+    queryParams["filters[age][\$lte]"] = max.toString();
+    queryParams["filters[age][\$gte]"] = min.toString();
+  }
+
+  return queryParams;
+}
+
+class AnimalSearchQueries {
+  AnimalSearchQueries({
+    required this.animalKind,
+    required this.size,
+    required this.hairLength,
+    required this.age,
+  });
+
+  final String animalKind;
+  final List<String>? size;
+  final List<String>? hairLength;
+  final List<Age>? age;
+
+  Map<String, dynamic> toQueries() {
+    return buildQueryParameter(animalKind, size: size, hairLength: hairLength, age: age?.toSet());
+  }
+
+  // equals
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is AnimalSearchQueries &&
+        other.animalKind == animalKind &&
+        listEquals(other.size, size) &&
+        listEquals(other.hairLength, hairLength) &&
+        listEquals(other.age, age);
+  }
+
+  @override
+  int get hashCode => animalKind.hashCode ^ size.hashCode ^ hairLength.hashCode ^ age.hashCode;
+
+}
--- anitection/lib/screens/search/animal_search_result_screen.dart
+++ anitection/lib/screens/search/animal_search_result_screen.dart
@@ -25,11 +25,11 @@ class AnimalSearchResultScreen extends ConsumerStatefulWidget {
 class AnimalSearchResultState extends ConsumerState<AnimalSearchResultScreen> {
   @override
   Widget build(BuildContext context) {
-    final searchResult = ref.watch(searchAnimalFutureProvider(buildQueryParameter(
-      widget.animalKind,
+    final searchResult = ref.watch(searchAnimalFutureProvider(AnimalSearchQueries(
+      animalKind: widget.animalKind,
       size: widget.size,
       hairLength: widget.hairLength,
-      age: widget.age?.toSet(),
+      age: widget.age,
     )));
     return Scaffold(
       body: searchResult.when(
@@ -59,53 +59,3 @@ class AnimalSearchResultState extends ConsumerState<AnimalSearchResultScreen> {
   }
 }
 
-Map<String, dynamic> buildQueryParameter(
-  String animalKind, {
-  List<String>? size,
-  List<String>? hairLength,
-  Set<Age>? age,
-}) {
-
-  final queryParams = {
-    "filters[animal_kind][name][\$eq]": animalKind,
-  };
-
-  if (size != null) {
-    for (int i = 0; i < size.length; i ++) {
-      final s = size[i];
-      queryParams["filters[\$or][$i][size][\$eq]"] = s;
-    }
-  }
-
-  if (hairLength != null) {
-    for (int i = 0; i < hairLength.length; i ++) {
-      final l = hairLength[i];
-      queryParams["filters[\$or][$i][hair_length][\$eq]"] = l;
-    }
-  }
-
-  if (age != null) {
-    int min;
-    int max;
-    if (age.contains(Age.kitten)) {
-      min = 0;
-    } else if (age.contains(Age.adult)) {
-      min = 1;
-    } else {
-      min = 6;
-    }
-
-    if (age.contains(Age.senior)) {
-      max = 2147483647;
-    } else if (age.contains(Age.adult)) {
-      max = 11;
-    } else {
-      max = 1;
-    }
-
-    queryParams["filters[age][\$lte]"] = max.toString();
-    queryParams["filters[age][\$gte]"] = min.toString();
-  }
-
-  return queryParams;
-}
