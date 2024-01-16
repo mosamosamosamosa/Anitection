809d90872848d26327d65ee233c3644a87be9886
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2024-01-16 09:22:23 +0900

    feat: 大画面であっても背景の模様が描画されるようにした

--- anitection/lib/components/animal_pad_background.dart
+++ anitection/lib/components/animal_pad_background.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -8,19 +7,21 @@ class AnimalPadBackground extends StatelessWidget {
   @override
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double dotSize = 50.0;
+    double space = 50.0;
+    double spaceY = 25.0;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final generateCount = (screenHeight / (dotSize)).ceil() * screenWidth / (dotSize).ceil();
     return Stack(
-      children: List.generate(100, (index) {
-        double screenWidth = MediaQuery.of(context).size.width;
-        double dotSize = 50.0;
-        double space = 50.0;
-        double spaceY = 25.0;
-
+      children: List.generate(generateCount.toInt(), (index) {
         int dotsPerRow = (screenWidth / (dotSize)).floor();
         int xIndex = index % dotsPerRow;
         int yIndex = (index / dotsPerRow).floor();
         bool isOddRow = yIndex.isOdd;
 
-        double xOffset = (dotSize + space) * xIndex + (isOddRow ? (space + dotSize) / 2 : 0);
+        double xOffset =
+            (dotSize + space) * xIndex + (isOddRow ? (space + dotSize) / 2 : 0);
         double yOffset = (dotSize + spaceY) * yIndex;
 
         return Positioned(
