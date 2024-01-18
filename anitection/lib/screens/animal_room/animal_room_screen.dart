61e3d69c971299288a66b40f2f780e4a3852f046
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2024-01-18 14:59:12 +0900

    feat: お出かけ先選択のダイアログを選択できるようにした

--- anitection/lib/screens/animal_room/animal_room_screen.dart
+++ anitection/lib/screens/animal_room/animal_room_screen.dart
@@ -1,3 +1,5 @@
+import 'package:anitection/components/normal_button.dart';
+import 'package:anitection/components/stroke_text.dart';
 import 'package:anitection/constants.dart';
 import 'package:anitection/models/animal/animal.dart';
 import 'package:anitection/models/base.dart';
@@ -177,12 +179,22 @@ class AnimalRoomScreenState extends ConsumerState<AnimalRoomScreen> {
     );
   }
 
-  void updateTabType(SelectedTab tabType) {
+  void updateTabType(SelectedTab tabType) async {
     if (selectedTab == SelectedTab.play) {
         ref.read(faceStateProvider.notifier).state = FaceStateType.sleeping;
         Future.delayed(const Duration(milliseconds: 5000), () {
           ref.read(faceStateProvider.notifier).state = FaceStateType.blink;
         });
+    }
+    if (tabType == SelectedTab.stroll) {
+      await showDialog(context: context, builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: StrollSelectionDialogLayout(),
+        );
+      });
     }
     setState(() {
       selectedTab = tabType;
@@ -278,7 +290,228 @@ class AnimalRoomBackground extends StatelessWidget {
 }
 
 
+class StrollSelectionDialogLayout extends StatelessWidget {
+  const StrollSelectionDialogLayout({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    const aspect = 0.36;
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: const Color(0xFFFFDB1D),
+            width: 5,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 16,),
+              const StrokeText(
+                text: "おでかけ先の選択",
+                strokeColor: Colors.white,
+                textColor: Color(0xFF573F1B),
+                textAlign: TextAlign.center,
+                strokeWidth: 4,
+                textStyle: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                shadows: [
+                  Shadow(
+                    blurRadius: 10,
+                    color: Colors.black,
+                    offset: Offset(2, 2),
+                  )
+                ],
+              ),
+              const SizedBox(height: 16,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        width: width * aspect,
+                        height: (width * aspect) * (90 / 120),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFA6EAFF),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Image.asset(
+                          "assets/images/img_park.png",
+                          fit: BoxFit.cover,
+                          width: width * aspect,
+                          height: (width * aspect) * (90 / 120),
+                        ),
+                      ),
+                      const Text(
+                          "公園",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF573F1B),
+                          )
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        width: width * aspect,
+                        height: (width * aspect) * (90 / 120),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Image.asset(
+                          "assets/images/img_amusement_park.png",
+                          fit: BoxFit.cover,
+                          width: width * aspect,
+                          height: (width * aspect) * (90 / 120),
+                        ),
+                      ),
+                      const Text(
+                          "遊園地",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF573F1B),
+                          )
+                      )
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(height: 16,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        width: width * aspect,
+                        height: (width * aspect) * (90 / 120),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Image.asset(
+                          "assets/images/img_shopping_street.png",
+                          fit: BoxFit.cover,
+                          width: width * aspect,
+                          height: (width * aspect) * (90 / 120),
+                        ),
+                      ),
+                      const Text(
+                          "商店街",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF573F1B),
+                          )
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        width: width * aspect,
+                        height: (width * aspect) * (90 / 120),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Image.asset(
+                          "assets/images/img_seaside.png",
+                          fit: BoxFit.cover,
+                          width: width * aspect,
+                          height: (width * aspect) * (90 / 120),
+                        ),
+                      ),
+                      const Text(
+                          "海辺",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF573F1B),
+                          )
+                      )
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(height: 16,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  NormalButton(
+                    onPressed: () {
+
+                    },
+                    width: 127,
+                    height: 55,
+                    maxElevation: 8,
+                    surfaceColor: const Color(0xFFC9C9C9),
+                    sideColor: const Color(0xFFA2A2A2),
+                    child: const StrokeText(
+                      text: "キャンセル",
+                      textColor: Color(0xFF573F1B),
+                      strokeColor: Colors.white,
+                      strokeWidth: 2,
+                      shadows: [
+                        Shadow(
+                          color: Color(0xFF573F1B),
+                          offset: Offset(2, 2),
+                          blurRadius: 10,
+                        ),
+                      ],
+                      textStyle: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  NormalButton(
+                    onPressed: () {
+
+                    },
+                    width: 127,
+                    height: 55,
+                    maxElevation: 8,
+                    surfaceColor: const Color(0xFFFFDB1D),
+                    sideColor: const Color(0xFFFFB001),
+                    child: const StrokeText(
+                      text: "出発",
+                      textColor: Color(0xFF573F1B),
+                      strokeColor: Colors.white,
+                      strokeWidth: 2,
+                      shadows: [
+                        Shadow(
+                          color: Color(0xFF573F1B),
+                          offset: Offset(2, 2),
+                          blurRadius: 10,
+                        ),
+                      ],
+                      textStyle: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16,),
+            ],
+          ),
+        )
+    );
+  }
+}
 
 enum SelectedTab {
   food, stroll, clean, play, normal,
